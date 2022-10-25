aab5c5ac5c42331dad16778056bfd3545356ae38
UegenePalimov <palbimov@mail.ru>
2022-10-26 01:20:19 +0500

    collectstatic v.3

--- team_work/settings.py
+++ team_work/settings.py
@@ -130,12 +130,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
