b838e0d8fdda004beb4bd42803d61e4810f241cc
UegenePalimov <palbimov@mail.ru>
2022-11-02 20:29:40 +0500

    Sprint-4 finally version

--- team_work/settings.py
+++ team_work/settings.py
@@ -12,13 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import json
 import os
 import urllib
-from os.path import dirname, abspath
 from pathlib import Path
 
-import whitenoise.middleware
 from dotenv import dotenv_values
 from dotenv import load_dotenv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,11 +128,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
